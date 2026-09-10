5aef07b8c0bbc5aa32b8fe84a9b2f5a7ae4ea6e0
Antonio <134897827+Antonio0707@users.noreply.github.com>
2026-09-10 11:11:22 -0400

    add smooth scrolling with js

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const botao = document.getElementById('botao-tema');
 const body = document.body;
 
 // Persistência do tema
+
 const temasalvo = localStorage.getItem('tema');
 temaEscuro(temasalvo === 'escuro');
 
